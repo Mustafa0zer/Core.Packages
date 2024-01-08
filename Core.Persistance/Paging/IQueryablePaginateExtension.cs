6f284491e6dd393272f8ffd6009011317d42906a
Mustafa0zer <mstfozer76@gmail.com>
2024-01-08 17:01:55 +0300

    Add project files.

---
+++ Core.Persistance/Paging/IQueryablePaginateExtension.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistance.Paging
+{
+    public static class IQueryablePaginateExtension
+    {
+        public static async Task<Paginate<T>> ToPaginateAsync<T>(this IQueryable<T> source,int index,int size,CancellationToken cancellationToken)
+        {
+            int count = await source.CountAsync(cancellationToken).ConfigureAwait(false);
+            List<T> items = await source.Skip(index*size).Take(size).ToListAsync(cancellationToken).ConfigureAwait(false);
+            Paginate<T> list = new()
+            {
+                Index = index,
+                Count = count,
+                Size = size,
+                Pages = (int)Math.Ceiling(count / (double)size)
+            };
+            return list;
+        }
+        public static Paginate<T> ToPaginate<T>(this IQueryable<T> source, int index, int size)
+        {
+            int count =  source.Count();
+            var items =  source.Skip(index * size).Take(size).ToList();
+            Paginate<T> list = new()
+            {
+                Index = index,
+                Count = count,
+                Size = size,
+                Pages = (int)Math.Ceiling(count / (double)size)
+            };
+            return list;
+        }
+    }
+}
---
+++ Core.Persistance/Repositories/DynamicQuery.cs
@@ -0,0 +1,23 @@
+﻿using Core.Persistance.Dynamic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistance.Repositories;
+
+public class DynamicQuery
+{
+    public IEnumerable<Sort>? Sort { get; set; }
+    public Filter? Filter { get; set; }
+    public DynamicQuery()
+    {
+       
+    }
+    public DynamicQuery(IEnumerable<Sort>? sort, Filter? filter)
+    {
+        Filter = filter;
+        Sort = sort; 
+    }
+}
