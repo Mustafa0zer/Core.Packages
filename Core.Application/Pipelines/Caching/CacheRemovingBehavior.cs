cf2252d95e77904a9b5cb23312ab7868dc3a759f
Mustafa0zer <mstfozer76@gmail.com>
2024-02-16 17:49:32 +0300

    ,

---
+++ Core.Application/Pipelines/Caching/CacheRemovingBehavior.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Caching;
+
+public class CacheRemovingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>, ICacheRemoverRequest
+{
+    private readonly IDistributedCache _cache;
+
+    public CacheRemovingBehavior(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if(request.BypassCache) 
+            return await next();
+
+        TResponse response = await next();
+
+        if(request.CacheGroupKey != null)
+        {
+            byte[]? cachedGroup = await _cache.GetAsync(request.CacheGroupKey, cancellationToken);
+            if (cachedGroup != null)
+            {
+                HashSet<string> keysInGroup=JsonSerializer.Deserialize<HashSet<string>>(Encoding.Default.GetString(cachedGroup));
+                foreach (var key in keysInGroup)
+                {
+                    await _cache.RemoveAsync(key,cancellationToken);
+                    //_logger.LogInformation($"Removed Cache -> {key}");
+                }
+                await _cache.RemoveAsync(request.CacheGroupKey, cancellationToken);
+                //_logger.LogInformation($"Removed Cache -> {request.CacheGroupKey}");
+                await  _cache.RemoveAsync(key: $"{request.CacheGroupKey}SlidingExpiration",cancellationToken);
+            }
+        }
+        
+        if(request.CachingKey != null)
+        {
+            await _cache.RemoveAsync(request.CachingKey,cancellationToken);
+        }
+        return response; 
+    }
+}
---
+++ Core.CrossCuttingConcerns/Serilog/Logger/FileLogger.cs
@@ -0,0 +1,32 @@
+﻿using Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Serilog.Logger;
+
+public class FileLogger:LoggerServiceBase
+{
+    private readonly IConfiguration _configuration;
+
+    public FileLogger(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        FileLogConfiguration logConfig = configuration.GetSection("SerilogLogConfigurations:FileLogConfiguration").Get<FileLogConfiguration>()
+            ??throw new Exception(SerilogMessages.NullOptionsMessage);
+
+        string logFilePath = string.Format(format:"{0}{1}",arg0:Directory.GetCurrentDirectory()+logConfig.FolderPath,arg1:".txt");
+        Logger  = new LoggerConfiguration().WriteTo.File(
+            logFilePath
+            ,rollingInterval:RollingInterval.Day //Hergün yeni Dosya oluştur.
+            ,retainedFileCountLimit:null //Eski Dosyaları Silmek için Limit var mı Hayır;
+            ,fileSizeLimitBytes:5000000 // dosya Mac Boyut
+            ,outputTemplate:"{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] {Message}{NewLine}{Excepiton}"
+            ).CreateLogger();
+    }
+}
