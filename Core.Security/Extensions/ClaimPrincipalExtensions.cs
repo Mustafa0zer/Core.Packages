6ec539d1705af2cdebda75d06740ccf03194b3eb
Mustafa0zer <mstfozer76@gmail.com>
2024-02-20 17:43:19 +0300

    token işlemleri yapıldı

---
+++ Core.Security/Extensions/ClaimPrincipalExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static List<string>? Claims(this ClaimsPrincipal claimsPrincipal, string claimType)
+    {
+        var result = claimsPrincipal?.FindAll(claimType)?.Select(x => x.Value).ToList();
+        return result;
+    }
+
+    public static List<string>? ClaimRoles(this ClaimsPrincipal claimsPrincipal) => claimsPrincipal?.Claims(ClaimTypes.Role);
+
+    public static int GetUserId(this ClaimsPrincipal claimsPrincipal) =>
+        Convert.ToInt32(claimsPrincipal?.Claims(ClaimTypes.NameIdentifier)?.FirstOrDefault());
+}
+
+
+//Claim Principal O anda Login olmuş claim sahibi kişi bilgileri.
