e1f508f17b99130e972241994251853ddd270a8f
Mustafa0zer <mstfozer76@gmail.com>
2024-02-21 13:40:23 +0300

    Jwt Entegrasyonu yazildi

---
+++ Core.Security/JWT/TokenOptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.JWT;
+
+public class TokenOptions
+{
+    public string Audience { get; set; } // hitap noktası
+    public string Issuer { get; set; } // token veren taraf
+    public int AccessTokenExpiration { get; set; } // süre
+    public string SecurityKey { get; set; } //güvenlik anahtarı
+    public int RefreshTokenTTL { get; set; } //TTL değeri for refresh
+
+    public TokenOptions()
+    {
+        Audience = string.Empty;
+        Issuer = string.Empty; 
+        SecurityKey = string.Empty;
+    }
+
+    public TokenOptions(string audience, string ıssuer, int accessTokenExpiration, string securityKey, int refreshTokenTTL)
+    {
+        Audience = audience;
+        Issuer = ıssuer;
+        AccessTokenExpiration = accessTokenExpiration;
+        SecurityKey = securityKey;
+        RefreshTokenTTL = refreshTokenTTL;
+    }
+}
